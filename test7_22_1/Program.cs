1a32d07f3630f00716626907e3ef71409924e778
vimoksa-web <qq2824509434@126.com>
2025-07-23 00:02:04 +0800

    Add files via upload

---
+++ test7_22_1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace test7_22_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.请用户输入姓名，年龄，班级
+            Console.WriteLine("请输入您的姓名");
+            Console.ReadLine();
+
+            Console.WriteLine("请输入您的年龄");
+            Console.ReadLine();
+
+            Console.WriteLine("请输入您的班级");
+            Console.ReadLine();
+
+            //2.问用户喜欢什么运动，无论用户输入什么，都回复"哈哈，好巧，我也喜欢这个运动"
+            Console.WriteLine("您喜欢什么运动");
+            Console.ReadLine();
+            Console.WriteLine("哈哈，好巧，我也喜欢这个运动");
+
+            //3.输入10*10的空心*矩形
+            Console.WriteLine("**********");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("**********");
+        }
+    }
+}
