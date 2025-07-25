fb530c5780d8e7e88b4f14706674446afded0611
vimoksa-web <qq2824509434@126.com>
2025-07-25 23:12:36 +0800

    Add files via upload

---
+++ test7_24/test7_24/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace test7_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.定义一个变量储存用户的姓名，然后在屏幕上显示"你好，XXX" XXX表示用户的姓名
+            //string Name = "XXX";
+            //Console.WriteLine("你好，" + Name);
+
+            //2.定义两个变量，一个储存客户的姓名，另一个储存年龄，然后在屏幕上显示："XXX + YYY岁了"
+            //string userName = "WXD";
+            //int userAge = 23;
+            //Console.WriteLine(userName + userAge + "岁了");
+
+            //3.当我们去面试时，前台会要求我们填一张表格，有姓名、年龄、邮箱、家庭住址、期望工资，请把这些信息在控制台输出
+            //string Name = "WXD";
+            //int Age = 23;
+            //string Email = "qq2824509434@126.com";
+            //string Address = "河南";
+            //int Money = 10000;
+            //Console.WriteLine("姓名：{0}\n年龄：{1}\n邮箱：{2}\n家庭住址：{3}\n期望工资：{4}\n", 
+            //    Name, Age, Email, Address, Money);
+
+            //4.请用户输入用户名，年龄，班级，最后一起用占位符的形式打印出来
+            //Console.WriteLine("请输入用户名：");
+            //string userName = Console.ReadLine();
+            //Console.WriteLine("请输入年龄：");
+            //string userAge = Console.ReadLine();
+            //Console.WriteLine("请输入班级：");
+            //string userClass = Console.ReadLine();
+            //Console.WriteLine("姓名：{0}，年龄：{1}，班级：{2}", userName, userAge, userClass);
+
+            //string str = string.Format("姓名：{0}，年龄：{1}，班级：{2}", userName, userAge, userClass);
+            //Console.WriteLine(str);
+        }
+    }
+}
