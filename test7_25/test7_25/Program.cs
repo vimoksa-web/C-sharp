cc021d74ce158ec6c9061330628ab4105019ce7e
vimoksa-web <qq2824509434@126.com>
2025-07-26 22:31:12 +0800

    Add files via upload

---
+++ test7_25/test7_25/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace test7_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.33 << 4 和 66 >> 1 的结果为
+            int a = 33; //100001 << 4 = 1000010000 = 16 + 512 = 528
+            int b = 66; //1000010 >> 1 = 100001 = 1 + 32 = 33
+            Console.WriteLine(a << 4);
+            Console.WriteLine(b >> 1);
+
+            //2.99 ^ 33 和 76 | 85的结果为
+            a = 99; 
+            //99 = 1100011
+            //33 = 0100001
+            //     1000010 = 66
+            b = 76; 
+            //76 = 1001100
+            //85 = 1010101
+            //     1011101 = 93
+            Console.WriteLine(a ^ 33);
+            Console.WriteLine(b | 85);
+        }
+    }
+}
---
+++ test7_25/test7_25_2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace test7_25_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.比较两个数的大小，求出最大的
+            //int a = 5;
+            //int b = 10;
+            //int i = a >= b ? a : b;
+            //Console.WriteLine(i);
+
+            //2.提示用户输入姓名，然后在控制台输出姓名，只要输入的不是帅哥，就显示美女
+            //Console.WriteLine("请输入姓名：");
+            //string strName = Console.ReadLine();
+            //string str = strName == "帅哥" ? "帅哥" : "美女";
+            //Console.WriteLine(str);
+
+            //3.依次输入学生的姓名、C#的成绩、Unity的成绩，两门成绩都大于等于90才能毕业，请输出最后的结果
+            //Console.WriteLine("请输入您的姓名：");
+            //string Name = Console.ReadLine();
+            //Console.WriteLine("请输入您的C#成绩：");
+            //int scoreCsharp = int.Parse(Console.ReadLine());
+            //Console.WriteLine("请输入您的姓名：");
+            //int scoreUnity = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(scoreCsharp >= 90 && scoreUnity >= 90 ? "可以毕业" : "不能毕业");
+
+            //4.要求用户输入一个年份，然后判断是不是闰年？
+            //闰年判断条件：年份能被400整除 或者 年份能被4整除，但是不能被100整除
+            Console.WriteLine("请输入一个年份：");
+            int Years = Convert.ToInt32(Console.ReadLine());
+
+            string isYear = Years % 400 == 0 || (Years % 4 == 0 && Years % 100 != 0) ? "是闰年" : "不是闰年";
+            Console.WriteLine(isYear);
+        }
+    }
+}
