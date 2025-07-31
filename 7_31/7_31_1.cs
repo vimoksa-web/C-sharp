116052d9790d16e5f013bc19d062e3b103680160
vimoksa-web <qq2824509434@126.com>
2025-07-31 22:26:00 +0800

    Add files via upload

---
+++ 7_31/7_31_1.cs
@@ -0,0 +1,65 @@
+﻿namespace Test_7_31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.通过W,A,S,D键在控制台中控制一个黄色方块在红色背景中上下左右移动
+            //注意：边界问题
+            //知识点：while switch 输入输出等
+
+            //改变背景颜色
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Clear();
+
+            //定义坐标变量
+            int x = 0, y = 0;
+
+            //设置字体颜色
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            //光标隐藏
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write("█");
+
+                //接收输入的按键，隐藏输入信息
+                char c = Console.ReadKey(true).KeyChar;
+
+                //刷新原来坐标的图形
+                Console.SetCursorPosition(x, y);
+                Console.Write(" ");
+                switch (c)
+                {
+                    case 'W':
+                    case 'w':
+                        y--;
+                        if (y < 0)
+                            y = 0;
+                        break;
+                    case 'A':
+                    case 'a':
+                        x -= 2;
+                        if (x < 0)
+                            x = 0;
+                        break;
+                    case 'S':
+                    case 's':
+                        y++;
+                        if ( y > Console.BufferHeight - 1)
+                            y = Console.BufferHeight - 1;
+                        break;
+                    case 'D':
+                    case 'd':
+                        x += 2;
+                        if ( x > Console.BufferWidth - 2)
+                            x = Console.BufferWidth - 2;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ 7_31/7_31_2.cs
@@ -0,0 +1,48 @@
+﻿namespace Test7_31_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.勇者打怪物
+            //勇者攻击力为8~12之间的一个值
+            //怪物防御为10，血量为20
+            //在控制台中通过打印信息表现勇者打怪物的过程
+            //描述怪物的掉血情况
+            //伤害计算公式：攻击力小于防御力时，减血为0，否则减血攻击力和防御力的差值
+            //知识点：循环，随机数
+
+            //定义怪物的防御力，血量
+            int Def = 10, Hp = 20;
+
+            while (true)
+            {
+                //生成勇者的攻击力
+                Random i = new Random();
+                int Atk = i.Next(8, 13);
+
+                //定义勇者造成的伤害
+                int Damage = Atk - Def;
+                if (Damage > 0)
+                {
+                    Hp = Hp - Damage;
+                    Console.WriteLine("勇者造成了{0}点伤害，怪物还剩{1}点血", Damage, Hp);
+                }
+                else
+                {
+                    Console.WriteLine("勇者没有造成伤害");
+                }
+
+                Console.WriteLine("请按任意键继续");
+                Console.ReadKey(true);
+                Console.Clear();
+
+                if (Hp <= 0)
+                {
+                    Console.WriteLine("勇者打倒了怪物");
+                    break;
+                }
+            }
+        }
+    }
+}
