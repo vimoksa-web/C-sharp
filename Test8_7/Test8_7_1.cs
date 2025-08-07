4ed2671c696f18d475fc68f1df1803236a6f43cf
vimoksa-web <qq2824509434@126.com>
2025-08-07 23:47:55 +0800

    Add files via upload

---
+++ Test8_7/Test8_7_1.cs
@@ -0,0 +1,85 @@
+﻿namespace Test8_7
+{
+    internal class Program
+    {
+
+        //1.int类型
+        static int Function1(int x, int y)
+        { 
+            return x > y ? x : y;
+        }
+        //float类型
+        static float Function1(float x, float y)
+        {
+            return x > y ? x : y;
+        }
+        //double类型
+        static double Function1(double x, double y)
+        {
+            return x > y ? x : y;
+        }
+
+
+        //2.int类型
+        static int Function2(params int[] arr)
+        {
+            if (arr.Length == 0)
+                return -1;
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+        //float类型
+        static float Function2(params float[] arr)
+        {
+            if (arr.Length == 0)
+                return -1;
+            float max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+        //double类型
+        static double Function2(params double[] arr)
+        {
+            if (arr.Length == 0)
+                return -1;
+            double max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            //1.请重载一个函数，让其可以比较两个int或两个float或两个double的大小，并返回较大的那个值
+            Console.WriteLine(Function1(6, 8));
+            Console.WriteLine(Function1(3.8f, 9.9f));
+            Console.WriteLine(Function1(9.8, 10.1));
+
+            //2.请重载一个函数，让其可以比较n个int或n个float或n个double的大小，并返回较大的那个值（用params可变参数来完成）
+            int[] arrInt = { 8, 10, 3, 9, 1 };
+            float[] arrFloat = { 1.1f, 2.3f, 3.9f, 0.8f, 9.1f };
+            double[] arrDouble = { 9.8, 10.2, 14.5, 5.6, 7.7 };
+
+            Console.WriteLine(Function2(arrInt));
+            Console.WriteLine(Function2(arrFloat));
+            Console.WriteLine(Function2(arrDouble));
+        }
+    }
+}
---
+++ Test8_7/Test8_7_2.cs
@@ -0,0 +1,90 @@
+﻿namespace Test8_7_2
+{
+    internal class Program
+    {
+        //1.
+        static void Fun(int a)
+        {
+            if (a > 10)
+                return;
+            Console.Write(a + " ");
+            a++;
+            Fun(a);
+        }
+
+        //2.
+        static int Factorial(int num)
+        {
+            if (num == 1)
+                return 1;
+            return num * Factorial(num - 1);
+        }
+
+        //3.
+        static void Sum(int x,  int sum)
+        {
+            if (x > 10)
+                return;
+            int mul = 1;
+            for (int i = 1; i <= x; i++)
+            {
+                mul *= i ;
+            }
+            sum += mul;
+            if (x == 10)
+                Console.WriteLine("1! + 2! + 3! + 4! + ...... + 10! = " + sum);
+            x++;
+            Sum(x, sum);
+        }
+
+        static int Function(int sum)
+        {
+            if (sum == 1)
+            {
+                return Factorial(1);
+            }
+            return Factorial(sum) + Function(sum - 1);
+        }
+
+        //4.
+        static void Long(float x, int day)
+        {
+            x /= 2f;
+            if (day == 10)
+            {
+                Console.WriteLine("第{0}天竹竿的长度为{1}", day, x);
+                return;
+            }
+            day++;
+            Long(x, day);
+        }
+
+        //5.
+        static bool Print(int x)
+        {
+            Console.WriteLine(x);
+            return x == 200 || Print(x + 1);
+        }
+        static void Main(string[] args)
+        {
+            //1.使用递归的方式打印0~10
+            Fun(0);
+            Console.WriteLine();
+
+            //2.传入一个值，递归求该值的阶乘，并返回
+            //5! = 1*2*3*4*5
+            int i = 10;
+            Console.WriteLine(i + "的阶乘为：" + Factorial(i));
+
+            //3.使用递归求1! + 2! + 3! + 4! + ...... + 10!
+            Sum(1, 0);
+            Console.WriteLine("1! + 2! + 3! + 4! + ...... + 10! = " + Function(10));
+
+            //4.一根竹竿长100m，每天砍掉一半，求第十天它的长度是多少（从第0天开始）
+            Long(100f, 0);
+
+            //5.不允许使用循环语句、条件语句，在控制台中打印出1~200这200个数（提示：递归 + 短路）
+            Print(1);
+        }
+    }
+}
