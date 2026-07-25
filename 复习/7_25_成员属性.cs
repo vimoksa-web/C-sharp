1e09c6be2f971d1844c30d27aadb8f1a3893967d
vimoksa-web <qq2824509434@126.com>
2026-07-25 20:22:12 +0800

    Add files via upload

---
+++ 复习/7_25_成员属性.cs
@@ -0,0 +1,88 @@
+﻿namespace _7_25
+{
+    #region 成员属性
+    //1.定义一个学生类，有五种属性，分别为姓名，性别，年龄，数学成绩，英语成绩
+    //有两个方法：
+    //一个打招呼：介绍自己叫XXX，今年几岁了，是男同学还是女同学
+    //一个计算自己总分和平均分并显示的方法
+    //使用属性完成，年龄必须是0~150岁之间，成绩必须是0~100
+    //实例化两个对象并测试
+    class Student
+    {
+        public string name { get; set; }
+        public char sex { get; set; }
+        private int age;
+        private int math;
+        private int english;
+        public int Age
+        {
+            get
+            {
+                return age; 
+            }
+            set
+            {
+                if (value < 0 || value > 150)
+                    throw new ArgumentException("年龄必须在0~150岁之间");
+                age = value;
+            }
+        }
+        public int Math 
+        { 
+            get
+            {
+                return math;
+            }
+            set
+            {
+                if (value < 0 || value > 100)
+                    throw new ArgumentException("成绩必须在0~100之间");
+                math = value;
+            } 
+        }
+        public int English
+        {
+            get
+            {
+                return english;
+            }
+            set
+            {
+                if (value < 0 || value > 100)
+                    throw new ArgumentException("成绩必须在0~100之间");
+                english = value;
+            }
+        }
+        public int Sum => this.Math + this.English;
+        public float Avg => this.Sum / 2f;
+        public void sayHi()
+        {
+            Console.WriteLine($"你好，我是{this.name}，今年{this.age}岁，性别{this.sex}");
+        }
+        public void Score()
+        {
+            Console.WriteLine($"{this.name}的总分为{this.Sum}，平均分为{this.Avg:F2}");
+        }
+    }
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 成员属性
+            Student s1 = new Student();
+            s1.name = "XD";
+            s1.Age = 23;
+            s1.sex = '男';
+            s1.Math = 90;
+            s1.English = 95;
+            s1.sayHi();
+            s1.Score();
+            Console.WriteLine("----------------------------");
+            Student s2 = new Student {name = "小红", Age = 24, sex = '女', Math = 80, English = 88 };
+            s2.sayHi();
+            s2.Score();
+            #endregion
+        }
+    }
+}
---
+++ 复习/7_25_索引器.cs
@@ -0,0 +1,102 @@
+﻿namespace _7_25_索引器
+{
+    #region 索引器
+    //1.自定义一个整型数组类，该类中有一个整型数组变量，
+    //为它封装增删改查的方法
+    class Number
+    {
+        public int[] array;
+        public int this[int index]
+        {
+            get
+            {
+                if (array == null || array.Length - 1 < index || index < 0)
+                    return 0;
+                return array[index];
+            }
+            set
+            {
+                if (array == null || array.Length - 1 < index || index < 0)
+                    throw new IndexOutOfRangeException("索引越界");
+                array[index] = value;
+            }
+        }
+        public void Add(int x)
+        {
+            if (array == null)
+            {
+                array = new int[] { x };
+                return;
+            }
+            int[] addArr = new int [array.Length + 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                addArr [i] = array [i];
+            }
+            addArr [addArr.Length - 1] = x;
+            this.array = addArr;
+        }
+        public void Del(int index)
+        {
+            if (array == null || array.Length - 1 < 0 || index < 0 || index >= array.Length)
+                return;
+            int[] delArr = new int[array.Length - 1];
+            int j = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == index)
+                    continue;
+                delArr[j] = array [i];
+                j++;
+            }
+            this.array = delArr;
+        }
+        public void Edit(int index, int x)
+        {
+            if (array == null || array.Length - 1 < 0 || index < 0 || index >= array.Length)
+                return;
+            array[index] = x;
+        }
+        public void Search(int x)
+        {
+            if (array == null || array.Length - 1 < 0)
+                return;
+            for (int i =0; i < array.Length; i++)
+            {
+                if (array[i] == x)
+                { 
+                    Console.WriteLine($"{x}所在的下标为{i}");
+                    return;
+                }
+            }
+            Console.WriteLine("没找到该元素");
+        }
+    }
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 索引器
+            //1.
+            Number n1 = new Number();
+            n1.Add(0);
+            n1.Add(1);
+            n1.Add(2);
+            n1.Add(3);
+            n1.Add(4);
+            n1.Add(5);
+            n1.Add(6);
+            n1.Add(7);
+            n1.Add(8);
+            n1.Add(9);
+            Console.WriteLine(string.Join(",", n1.array));
+            n1.Del(0);
+            Console.WriteLine(string.Join(",", n1.array));
+            n1.Edit(0, 10);
+            Console.WriteLine(string.Join(",", n1.array));
+            n1.Search(1);
+            #endregion
+        }
+    }
+}
