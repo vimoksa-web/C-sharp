3c836594760456227a1eafd6292e4a301027aae8
vimoksa-web <qq2824509434@126.com>
2026-07-28 23:30:53 +0800

    Add files via upload

---
+++ 面向对象——封装/9.拓展方法.cs
@@ -0,0 +1,74 @@
+﻿namespace _9.拓展方法
+{
+    #region 拓展方法
+    //1.为整型拓展一个求平方的方法
+    static class Tools
+    {
+        public static int Square(this int s)
+        {
+            return s * s;
+        }
+        public static void Suicide(this Player p)
+        {
+            p.Hurt(10000);
+        }
+    }
+    //2.写一个玩家类，包含姓名、血量、攻击力、防御力等特征，移动，受伤等方法，
+    //为该玩家类拓展一个自杀的方法
+    class Player
+    {
+        private string name;
+        private int hp;
+        private int atk;
+        private int def;
+        private int coordinates;
+        public Player() : this("无名氏") { }
+        public Player(string playerName)
+        {
+            name = playerName;
+            hp = 100;
+            atk = 5;
+            def = 5;
+            coordinates = 0;
+        }
+        public void Move(int i)
+        {
+            coordinates += i;
+            Console.WriteLine($"{name}移动了{i}米,一共移动了{coordinates}米");
+        }
+        public void Hurt(int a)
+        {
+            int damage = a - def;
+            if (damage < 0)
+                damage = 0;
+            hp -= damage;
+
+            if (hp > 0)
+            {
+                Console.WriteLine($"{name}受到了攻击，剩余血量{hp}");
+            }
+            else
+            {
+                Console.WriteLine("你死了");
+            }
+        }
+    }
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 拓展方法
+            //1.
+            int i = 10;
+            Console.WriteLine(i.Square());
+
+            //2.
+            Player p1 = new Player();
+            p1.Move(5);
+            p1.Hurt(10);
+            p1.Suicide();
+            #endregion
+        }
+    }
+}
---
+++ 面向对象——封装/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace _10.运算符重载
+{
+    #region 运算符重载
+    //1.定义一个位置结构体或类，为其重载判断是否相同的运算符
+    //(x1,y1)==(x2,y2)   =>   两个值相等时才为true
+    struct Point
+    {
+        public int x;
+        public int y;
+        public static bool operator ==(Point a, Point b)
+        {
+            if (a.x == b.x && a.y == b.y)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Point a, Point b)
+        {
+            return !(a == b);
+        }
+    }
+
+    //2.定义一个Vector类(x,y,z)，通过重载运算符实现以下运算：
+    //(x1,y1,z1) + (x2,y2,z2) = (x1+x2,y1+y2,z1+z2)
+    //(x1,y1,z1) - (x2,y2,z2) = (x1-x2,y1-y2,z1-z2)
+    //(x1,y1,z1) * num = (x1*num,y1*num,z1*num)
+    class Vector
+    {
+        public int x;
+        public int y;
+        public int z;
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            Vector v = new Vector();
+            v.x = v1.x + v2.x;
+            v.y = v1.y + v2.y;
+            v.z = v1.z + v2.z;
+            return v;
+        }
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            Vector v = new Vector();
+            v.x = v1.x - v2.x;
+            v.y = v1.y - v2.y;
+            v.z = v1.z - v2.z;
+            return v;
+        }
+        public static Vector operator *(Vector v1, int num)
+        {
+            Vector v = new Vector();
+            v.x = v1.x * num;
+            v.y = v1.y * num;
+            v.z = v1.z * num;
+            return v;
+        }
+    }
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 运算符重载
+            //1.
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 1;
+            Point p2 = new Point();
+            p2.x = 1;
+            p2.y = 1;
+            if (p1 == p2)
+                Console.WriteLine("p1==p2");
+            else
+                Console.WriteLine("p1!=p2");
+
+            //2.
+            Vector v1 = new Vector();
+            v1.x = 5;
+            v1.y = 5;
+            v1.z = 5;
+            Vector v2 = new Vector();
+            v2.x = 2;
+            v2.y = 2;
+            v2.z = 2;
+
+            int num = 3;
+            Vector v3 = new Vector();
+
+            v3 = v1 + v2;
+            Console.WriteLine($"{v3.x}, {v3.y}, {v3.z}");
+            v3 = v1 - v2;
+            Console.WriteLine($"{v3.x}, {v3.y}, {v3.z}");
+            v3 = v1 * num;
+            Console.WriteLine($"{v3.x}, {v3.y}, {v3.z}");
+            #endregion
+        }
+    }
+}
