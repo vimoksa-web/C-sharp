f4704873b64f1540e3ef0c1323ec6e36138b71fb
vimoksa-web <qq2824509434@126.com>
2026-07-30 00:06:05 +0800

    Add files via upload

---
+++ 面向对象——继承/4.万物之父和装箱拆箱.cs
@@ -0,0 +1,35 @@
+﻿namespace _4.万物之父和装箱拆箱
+{
+    //1.请口头描述什么是装箱和拆箱
+    //装箱：用object类存储值类型
+    //拆箱：把存储的object类转换为值类型
+
+    
+    internal class Program
+    {
+        //2.请用代码描述装箱拆箱
+        //拆箱
+        static int[] Unboxing(object[] o)
+        {
+            if (o == null || o.Length == 0)
+                return null;
+            int[] arr = new int[o.Length];
+            for (int i = 0; i < o.Length; i++)
+            {
+                arr[i] = (int)o[i];
+            }
+            return arr;
+        }
+        static void Main(string[] args)
+        {
+            //装箱
+            Random r = new Random();
+            object[] o = new object[10];
+            for (int i = 0; i < o.Length; ++i)
+            {
+                o[i] = r.Next(0, 11);
+            }
+            Console.WriteLine(string.Join(",", Unboxing(o)));
+        }
+    }
+}
---
+++ 面向对象——继承/5.密封类.cs
@@ -0,0 +1,127 @@
+﻿namespace _5.密封类
+{
+    //1.定义一个载具类，有速度、最大速度、可乘人数、司机和乘客等，
+    //有上车、下车、行驶、车祸等方法，用载具类声明一个对象，并将若干人装载上车
+    //载具类
+    class Vehicle
+    {
+        public float speed {  get; protected set; }
+        public int maxSpeed { get; protected set; }
+        public int capacity { get; protected set; }
+        public string driver { get; protected set; }
+        public Passenger[] passengers { get; protected set; }
+        private bool isSpeeding;
+        public Vehicle(Passenger[] passengers)
+        {
+            speed = 0;
+            maxSpeed = 120;
+            capacity = 30;
+            driver = "王师傅";
+            this.passengers = passengers;
+            isSpeeding = false;
+        }
+        //上车
+        public void GetOn(Passenger passenger)
+        {
+            if (passenger == null) return;
+            if (passengers.Length >= capacity)
+            {
+                Console.WriteLine("最大容量30人，没有空位了");
+                return;
+            }
+            Passenger[] arr = new Passenger[passengers.Length + 1];
+            for (int i = 0; i < passengers.Length; i++)
+            {
+                arr[i] = passengers[i];
+            }
+            arr[passengers.Length] = passenger;
+            passengers = arr;
+        }
+        //下车
+        public void GetOff()
+        {
+            if (passengers == null || passengers.Length == 0)
+            {
+                Console.WriteLine("车上没有乘客");
+                return;
+            }
+            Passenger[] arr = new Passenger[passengers.Length - 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = passengers[i];
+            }
+            passengers = arr;
+        }
+        //行驶
+        public void Driving(int time)
+        {
+            speed = 1.2f * time;
+            if (speed >= maxSpeed)
+            {
+                speed = maxSpeed;
+                Console.WriteLine($"已达最大车速{maxSpeed}");
+            }
+            if (speed > 100)
+            {
+                isSpeeding = true;
+                Console.WriteLine($"当前车速为{speed:F2}，您已超速");
+                return;
+            }
+            else
+                isSpeeding = false;
+            Console.WriteLine($"经过{time}分钟，当前车速为{speed:F2}");
+        }
+        //车祸
+        public void Accident()
+        {
+            if (isSpeeding)
+            {
+                Console.WriteLine("出车祸了");
+                if (passengers == null || passengers.Length == 0)
+                {
+                    Console.WriteLine("车上没有乘客，没有伤亡");
+                    return;
+                }
+                Passenger[] arr = new Passenger[passengers.Length / 2];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = passengers[i];
+                }
+                passengers = arr;
+                Console.WriteLine("车上伤亡一半人");
+            }
+            else
+            {
+                Console.WriteLine("遵纪守法，没有发生车祸");
+            }
+        }
+    }
+    //乘客
+    class Passenger
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Passenger p1 = new Passenger();
+            Passenger p2 = new Passenger();
+            Passenger p3 = new Passenger();
+            Passenger p4 = new Passenger();
+            Passenger p5 = new Passenger();
+            Passenger p6 = new Passenger();
+            Passenger p7 = new Passenger();
+            Passenger p8 = new Passenger();
+            Passenger p9 = new Passenger();
+            Passenger p10 = new Passenger();
+            Passenger[] ps = { p1, p2, p3, p4, p5, p6, p7, p8, p9, p10 };
+            Passenger p11 = new Passenger();
+            Vehicle v1 = new Vehicle(ps);
+            v1.GetOn(p11);
+            v1.GetOff();
+            v1.Driving(60);
+            v1.Accident();
+        }
+    }
+}
